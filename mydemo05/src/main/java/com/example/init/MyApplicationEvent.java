280fc95c33151a89854ee312d1142683b2887778
aabbcc5050 <aabbcc5050@qq.com>
2017-04-19 14:04:13 +0800

    commit test

--- mydemo05/src/main/java/com/example/init/MyApplicationEvent.java
+++ mydemo05/src/main/java/com/example/init/MyApplicationEvent.java
@@ -2,8 +2,15 @@ package com.example.init;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent;
+import org.springframework.boot.context.event.ApplicationPreparedEvent;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.ContextStartedEvent;
+import org.springframework.context.event.ContextStoppedEvent;
 import org.springframework.stereotype.Component;
 
 /**
@@ -25,8 +32,31 @@ public class MyApplicationEvent implements ApplicationListener<ApplicationEvent>
 
 	@Override
 	public void onApplicationEvent(ApplicationEvent event) {
-		System.out.println("##########################################################");
-		log.info(event.getClass().getSimpleName());
+		// 在这里可以监听到Spring Boot的生命周期
+        if (event instanceof ApplicationEnvironmentPreparedEvent) { // 初始化环境变量 
+        	log.info("*****->[{}]","初始化环境变量");
+        }
+        else if (event instanceof ApplicationPreparedEvent) { // 初始化完成 
+        	log.info("*****->[{}]"," 初始化完成 ");
+        }
+        else if (event instanceof ContextRefreshedEvent) { // 应用刷新 
+        	log.info("*****->[{}]","应用刷新 ");
+        }
+        else if (event instanceof ApplicationReadyEvent) {// 应用已启动完成 
+        	log.info("*****->[{}]","应用已启动完成");
+        }
+        else if (event instanceof ContextStartedEvent) { // 应用启动，需要在代码动态添加监听器才可捕获 
+        	log.info("*****->[{}]","应用启动，需要在代码动态添加监听器才可捕获 ");
+        }
+        else if (event instanceof ContextStoppedEvent) { // 应用停止 
+        	log.info("*****->[{}]","应用停止 ");
+        }
+        else if (event instanceof ContextClosedEvent) { // 应用关闭 
+        	log.info("*****->[{}]","应用关闭 ");
+        }
+        else {
+        	log.info("*****[{}]->[{}]","其他",event.getSource().getClass().getSimpleName());
+        }
 		
 	}
 
