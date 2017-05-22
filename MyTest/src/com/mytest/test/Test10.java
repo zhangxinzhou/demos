034b40c7b1dbbe6bb0780ef9190e6f7955df9da1
aabbcc5050 <aabbcc5050@qq.com>
2017-05-22 14:04:53 +0800

    111

---
+++ MyTest/src/com/mytest/test/Test10.java
@@ -0,0 +1,42 @@
+package com.mytest.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test10 {
+
+	private static ThreadLocal<Long> count=new ThreadLocal<>();
+	
+	public static void main(String[] args) {
+		int num=10;
+		test(num);
+	}
+	
+	/**
+	 * n个学生围城一圈,从第0个学生开始报数,报数能被3整除的学生将退出圈,循环多次之后最后会剩下那个学生
+	 * 1.先遍历一次圈,遍历完成之后再把能被3整除的学生T出圈
+	 * 2.然后循环遍历上一步剩下的圈,直到圈的大小为1
+	 * @param num
+	 */
+	public static void test(int num){	
+		//数据准备
+		List<Integer> list=new ArrayList<>(num);
+		List<Integer> remove=new ArrayList<>();
+		for (int i = 0; i < num; i++) {
+			list.add(i);
+		}
+		count.set(0L);
+		//开始遍历
+		System.out.println(list);
+		while(list.size()>1){			
+			list.forEach(stu->{
+				count.set(count.get()+1);
+				if(count.get()%3==0){remove.add(stu);}
+			});			
+			list.removeAll(remove);
+			remove.clear();
+			System.out.println(list);
+		}	
+		System.out.println("报数报到 : "+count.get());
+	}
+}
