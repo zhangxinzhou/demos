7089a71d4c2b36aa7dc8fdd5fe27fa32103b5be9
aabbcc5050 <aabbcc5050@qq.com>
2017-05-22 15:02:28 +0800

    111

--- MyTest/src/com/mytest/test/Test10.java
+++ MyTest/src/com/mytest/test/Test10.java
@@ -8,22 +8,24 @@ public class Test10 {
 	private static ThreadLocal<Long> count=new ThreadLocal<>();
 	
 	public static void main(String[] args) {
-		int num=10;
-		test(num);
-		test1(num);
+		int m=100;
+		int n=3;
+		test(m,n);
+		test1(m,n);
 	}
 	
 	/**
-	 * n个学生围城一圈,从第0个学生开始报数,报数能被3整除的学生将退出圈,循环多次之后最后会剩下那个学生
+	 * m个学生围城一圈,从第0个学生开始报数,报数能被n整除的学生将退出圈,循环多次之后最后会剩下那个学生
 	 * 1.先遍历一次圈,遍历完成之后再把能被3整除的学生T出圈
 	 * 2.然后循环遍历上一步剩下的圈,直到圈的大小为1
-	 * @param num
+	 * @param m   学生人数
+	 * @param n   被除数
 	 */
-	public static void test(int num){	
+	public static void test(int m,int n){	
 		//数据准备
-		List<Integer> list=new ArrayList<>(num);
-		List<Integer> remove=new ArrayList<>();
-		for (int i = 0; i < num; i++) {
+		List<Integer> list=new ArrayList<>(m);
+		List<Integer> remove=new ArrayList<>(m/n);
+		for (int i = 0; i < m; i++) {
 			list.add(i);
 		}
 		count.set(0L);
@@ -32,7 +34,7 @@ public class Test10 {
 		while(list.size()>1){			
 			list.forEach(stu->{
 				count.set(count.get()+1);
-				if(count.get()%3==0){remove.add(stu);}
+				if(count.get()%n==0){remove.add(stu);}
 			});			
 			list.removeAll(remove);
 			remove.clear();
@@ -42,11 +44,11 @@ public class Test10 {
 	}
 	
 	
-	public static void test1(int num){	
+	public static void test1(int m,int n){	
 		//数据准备
-		List<Integer> list=new ArrayList<>(num);
-		List<Integer> remove=new ArrayList<>();
-		for (int i = 0; i < num; i++) {
+		List<Integer> list=new ArrayList<>(m);
+		List<Integer> remove=new ArrayList<>(m/n);
+		for (int i = 0; i < m; i++) {
 			list.add(i);
 		}
 		int count=0;
@@ -55,7 +57,7 @@ public class Test10 {
 		while(list.size()>1){			
 			for (Integer integer : list) {
 				count++;
-				if(count%3==0){remove.add(integer);}
+				if(count%n==0){remove.add(integer);}
 			}		
 			list.removeAll(remove);
 			remove.clear();
