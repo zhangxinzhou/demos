1aaa29526a62f3266c8444ed564a1ad6bcd6e4a2
aabbcc5050 <aabbcc5050@qq.com>
2017-06-02 20:04:38 +0800

    110

--- nginx+tomcats+redis/springboot/demo2/src/main/java/com/example/config/RedisSessionConfig.java
+++ nginx+tomcats+redis/springboot/demo2/src/main/java/com/example/config/RedisSessionConfig.java
@@ -1,16 +1,10 @@
 package com.example.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds=60)//session过期时间
 @Configuration
 public class RedisSessionConfig {
 
-	@Bean
-	public JedisConnectionFactory jedisConnectionFactory(){
-		return new JedisConnectionFactory();
-	}
 }
