44adc4687aa09845cc245faad3862a7c074b3ef7
aabbcc5050 <aabbcc5050@qq.com>
2017-06-22 22:21:23 +0800

    vue.js的学习

---
+++ vue.js的学习/vue.js/src/main/java/com/example/controller/WebController.java
@@ -0,0 +1,29 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+	
+	@RequestMapping("/index")
+	public String index(){
+		return "index";
+	}
+	
+	@RequestMapping("/demo1")
+	public String demo1(){
+		return "demo1";
+	}
+	
+	@RequestMapping("/demo2")
+	public String demo2(){
+		return "demo2";
+	}
+	
+	@RequestMapping("/demo3")
+	public String demo3(){
+		return "demo3";
+	}
+}
