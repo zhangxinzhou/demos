7eb06b22134c26637cdad093013ce139282c9ccf
aabbcc5050 <aabbcc5050@qq.com>
2018-10-28 19:51:38 +0800

    AAA

--- MyTest/src/com/mytest/test/Test05.java
+++ MyTest/src/com/mytest/test/Test05.java
@@ -3,8 +3,8 @@ package com.mytest.test;
 public class Test05 {
 
 	public static void main(String[] args) {
-		String str="111";
-		if(str.getClass()==String.class){
+		String str = "111";
+		if (str.getClass() == String.class) {
 			System.out.println("lll");
 		}
 	}
